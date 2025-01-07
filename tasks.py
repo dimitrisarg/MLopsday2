62c0c921ea0c2d77cb18696a972f7a350b5009be
dimitrisarg <argidimitris@gmail.com>
2025-01-07 16:56:06 +0100

    with invoke

--- tasks.py
+++ tasks.py
@@ -76,3 +76,14 @@ def build_docs(ctx: Context) -> None:
 def serve_docs(ctx: Context) -> None:
     """Serve documentation."""
     ctx.run("mkdocs serve --config-file docs/mkdocs.yaml", echo=True, pty=not WINDOWS)
+
+@task
+def python(ctx):
+    """ """
+    ctx.run("which python" if os.name != "nt" else "where python")
+
+@task
+def git(ctx, message):
+    ctx.run(f"git add .")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
